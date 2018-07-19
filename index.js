7050f7c008a601fbc981dafc540fff77f6aebdd3
Abba Yosua <email@abbayosua.com>
2018-07-19 16:37:32 +0700

    Add Figma web Mockup, Fix CSS for FORM

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function addactivity() {
     var textnode = document.createTextNode("Doing Good yeeeeeeeeeeeeeeeeeeeeee");
     node.appendChild(textnode);
     document.getElementById("activities").appendChild(node);
+    event.preventDefault();
 }
