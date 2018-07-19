d7884e1c0607f1f571bebd16670b20110f85f829
Abba Yosua <email@abbayosua.com>
2018-07-19 10:02:33 +0700

    Updated Interface

---
+++ index.js
@@ -0,0 +1,6 @@
+function addactivity() {
+    var node = document.createElement("DIV");
+    var textnode = document.createTextNode("Doing Good yeeeeeeeeeeeeeeeeeeeeee");
+    node.appendChild(textnode);
+    document.getElementById("activities").appendChild(node);
+}
