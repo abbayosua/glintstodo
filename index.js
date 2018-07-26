eae2c09362c584f7fe998cbe5caf796f71f55aff
Abba Yosua <email@abbayosua.com>
2018-07-26 16:32:08 +0700

    Added suggestion fix from Arie

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function addactivity() {
     let inputfromuser = document.getElementById("activityInput")
 
     let node = document.createElement("DIV");
+    node.className = "hello "
     let textnode = document.createTextNode(inputfromuser.value);
     node.appendChild(textnode);
     document.getElementById("activities").appendChild(node);
